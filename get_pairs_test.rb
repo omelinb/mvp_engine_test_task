a1389226580d5cef17aa2c738392a2ddadb298f9
omelinb <elros.ob@gmail.com>
2020-05-02 01:42:10 +0300

    Removed two tests

--- get_pairs_test.rb
+++ get_pairs_test.rb
@@ -7,7 +7,5 @@ class GetPairsTest < Test::Unit::TestCase
     assert_equal([[-5, 10], [2, 3], [2, 3], [5, 0]], get_pairs([-5, 33, 2, 2, 3, 5, 0, 10, 3]))
     assert_equal([[5, 0], [5, 0], [5, 0]], get_pairs([5, 5, 5, 0, 0, 0, 5]))
     assert_equal([[3, 3], [6, 0]], get_pairs([3, 3, 6, 0], 6))
-    assert_equal([], get_pairs([1, 3, 7, 0]))
-    assert_equal([], get_pairs([]))
   end
 end
